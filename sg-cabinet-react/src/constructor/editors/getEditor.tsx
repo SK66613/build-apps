f2ddcb972d37fb8b56bd95e642475164b80ac928
SK66613 <cyberian13@icloud.com>
2026-02-05 21:58:33 +0300

    Create getEditor.tsx

---
+++ sg-cabinet-react/src/constructor/editors/getEditor.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BonusWheelEditor from './BonusWheelEditor';
+import StarsProductEditor from './StarsProductEditor';
+import JsonPropsEditor from './JsonPropsEditor';
+
+// сюда потом добавим styles_passport_one / game_flappy_one / calendar_booking_one и т.д.
+export function getEditorForKey(key: string){
+  if (key === 'bonus_wheel_one') return BonusWheelEditor;
+  if (key === 'shop_stars_product_one') return StarsProductEditor;
+  return JsonPropsEditor;
+}
